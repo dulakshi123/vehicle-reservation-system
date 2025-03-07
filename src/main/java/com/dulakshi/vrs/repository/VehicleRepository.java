a9bc8bbe53bba4f0a8af83ac32958a7037cb9048
dulakshi123 <dulakshiwijenayaka@gmail.com>
2025-03-07 22:01:53 +0530

    Update vehicle service and repository

--- src/main/java/com/dulakshi/vrs/repository/VehicleRepository.java
+++ src/main/java/com/dulakshi/vrs/repository/VehicleRepository.java
@@ -3,5 +3,8 @@ package com.dulakshi.vrs.repository;
 import com.dulakshi.vrs.entity.Vehicle;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
+    Optional<Vehicle> findVehicleByVrn(String vrn);
 }
--- src/main/java/com/dulakshi/vrs/service/VehicleServiceImpl.java
+++ src/main/java/com/dulakshi/vrs/service/VehicleServiceImpl.java
@@ -1,24 +1,67 @@
 package com.dulakshi.vrs.service;
 
 import com.dulakshi.vrs.dto.VehicleDTO;
+import com.dulakshi.vrs.entity.Model;
+import com.dulakshi.vrs.entity.Status;
+import com.dulakshi.vrs.entity.Vehicle;
+import com.dulakshi.vrs.repository.VehicleRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
+@AllArgsConstructor
 public class VehicleServiceImpl implements VehicleService {
+    @Autowired
+    private final VehicleRepository vehicleRepository;
+
     @Override
     public VehicleDTO addNewVehicle(VehicleDTO vehicleDTO) {
-        return null;
+        Model vehicleType = Model.getModel(vehicleDTO.getType());
+
+        Vehicle vehicle = new Vehicle();
+        vehicle.setVrn(vehicleDTO.getVrn());
+        vehicle.setModel(vehicleType);
+        vehicle.setStatus(Status.AVAILABLE);
+        vehicle.setSeatCount(vehicleType.getPassengerCount());
+
+        Vehicle savedVehicle = vehicleRepository.save(vehicle);
+
+        vehicleDTO.setId(savedVehicle.getId());
+        vehicleDTO.setStatus(vehicle.getStatus().name());
+        vehicleDTO.setSeatCount(savedVehicle.getSeatCount());
+        return vehicleDTO;
     }
 
     @Override
     public boolean updateVehicleStatus(Long vehicleId, String status) {
-        return false;
+        Optional<Vehicle> optionalVehicle = vehicleRepository.findById(vehicleId);
+
+        if(optionalVehicle.isEmpty()) return false;
+
+        Vehicle vehicle = optionalVehicle.get();
+        try {
+            vehicle.setStatus(Status.getStatus(status));
+            vehicleRepository.save(vehicle);
+            return true;
+        } catch (IllegalArgumentException e) {
+            return false;
+        }
     }
 
     @Override
     public List<VehicleDTO> getAllVehicles() {
-        return List.of();
+        return vehicleRepository.findAll().stream().map(vehicle -> {
+            VehicleDTO vehicleDTO = new VehicleDTO();
+            vehicleDTO.setId(vehicle.getId());
+            vehicleDTO.setVrn(vehicle.getVrn());
+            vehicleDTO.setType(vehicle.getModel().name());
+            vehicleDTO.setStatus(vehicle.getStatus().name());
+            vehicleDTO.setSeatCount(vehicle.getSeatCount());
+            return vehicleDTO;
+        }).toList();
     }
 }
