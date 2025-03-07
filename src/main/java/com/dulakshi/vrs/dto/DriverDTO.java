be88c038f381a269909b869b96d68c3a9551798e
dulakshi123 <dulakshiwijenayaka@gmail.com>
2025-03-07 17:44:36 +0530

    Create DTO class for Driver

---
+++ src/main/java/com/dulakshi/vrs/dto/DriverDTO.java
@@ -0,0 +1,13 @@
+package com.dulakshi.vrs.dto;
+
+import lombok.Data;
+
+@Data
+public class DriverDTO {
+    private Long id;
+    private String name;
+    private String mobile;
+    private String dln;
+    private String regId;
+    private String status;
+}
