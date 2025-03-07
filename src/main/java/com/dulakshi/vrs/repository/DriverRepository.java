dd28b77da9a695006a5ad33f1fa5585b88642226
dulakshi123 <dulakshiwijenayaka@gmail.com>
2025-03-07 17:50:08 +0530

    Create Driver Repository

---
+++ src/main/java/com/dulakshi/vrs/repository/DriverRepository.java
@@ -0,0 +1,7 @@
+package com.dulakshi.vrs.repository;
+
+import com.dulakshi.vrs.entity.Driver;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DriverRepository extends JpaRepository<Driver, Long> {
+}
