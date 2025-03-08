465c9dbf12c6a8d834daa4df65102dbf419ed69b
dulakshi123 <dulakshiwijenayaka@gmail.com>
2025-03-08 13:37:44 +0530

    Add new method

--- src/main/java/com/dulakshi/vrs/repository/UserRepository.java
+++ src/main/java/com/dulakshi/vrs/repository/UserRepository.java
@@ -3,5 +3,8 @@ package com.dulakshi.vrs.repository;
 import com.dulakshi.vrs.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findUserByEmail(String email);
 }
