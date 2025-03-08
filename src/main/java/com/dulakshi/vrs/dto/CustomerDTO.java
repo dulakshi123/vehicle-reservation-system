738e328d7b06a1ac3f8c9a437aee9a08ffbfb92a
dulakshi123 <dulakshiwijenayaka@gmail.com>
2025-03-08 15:48:57 +0530

    Add new fields

--- src/main/java/com/dulakshi/vrs/dto/CustomerDTO.java
+++ src/main/java/com/dulakshi/vrs/dto/CustomerDTO.java
@@ -4,6 +4,8 @@ import lombok.Data;
 
 @Data
 public class CustomerDTO {
+    private Long id;
+    private String regId;
     private String name;
     private String email;
     private String mobile;
