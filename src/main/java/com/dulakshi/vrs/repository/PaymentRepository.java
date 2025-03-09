c1ca03bcec34a4fec42d7bf663caec13da705f4b
dulakshi123 <dulakshiwijenayaka@gmail.com>
2025-03-09 13:52:57 +0530

    Update payment repository

--- src/main/java/com/dulakshi/vrs/repository/PaymentRepository.java
+++ src/main/java/com/dulakshi/vrs/repository/PaymentRepository.java
@@ -3,5 +3,8 @@ package com.dulakshi.vrs.repository;
 import com.dulakshi.vrs.entity.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
+    Optional<Payment> findPaymentByReservation_Id(Long reservationId);
 }
